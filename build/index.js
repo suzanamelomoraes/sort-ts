c6e5f016421dd41241960554332a8c2749b8315a
Suzana Melo <suzanamelojor@gmail.com>
2022-02-03 12:09:40 +1300

    Sort project: first attempt to write a sort method

--- build/index.js
+++ build/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("hello there");
+class Sorter {
+    // collection: number[];
+    // constructor(collection: number[]){
+    //   this.collection = collection;
+    // }
+    // Same as above
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
