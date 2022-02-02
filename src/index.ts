e9560cd5f92f3838eb7975dbb72064274b7e2b1c
Suzana Melo <suzanamelojor@gmail.com>
2022-02-03 11:51:45 +1300

    Sort project: create sorter class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello there")
+class Sorter {
+  // collection: number[];
+
+  // constructor(collection: number[]){
+  //   this.collection = collection;
+  // }
+
+  // Same as above
+  constructor(public collection: number[]){
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0])
