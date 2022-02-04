09c62d93e478f81f756f0aaa7ab3f9dbad189a0f
Suzana Melo <suzanamelojor@gmail.com>
2022-02-04 16:40:41 +1300

    Sort project: create ts file called sorter and move sorter crss from index to this new file

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,5 @@
-class Sorter {
-  // collection: number[];
+import { Sorter } from "./Sorter";
 
-  // constructor(collection: number[]){
-  //   this.collection = collection;
-  // }
-
-  // Same as above
-  constructor(public collection: number[]){
-  }
-
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]){
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
 
 const sorter = new Sorter([10, 3, -5, 0])
 sorter.sort();
