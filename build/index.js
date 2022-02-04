86d8c3df8e4cb0b93cde1a8b1a0edaa78ee1f22d
Suzana Melo <suzanamelojor@gmail.com>
2022-02-04 17:30:11 +1300

    Sort project: update index to use sorter and numberscollection accordingly

--- build/index.js
+++ build/index.js
@@ -1,40 +1,8 @@
 "use strict";
-class Sorter {
-    // collection: number[];
-    // constructor(collection: number[]){
-    //   this.collection = collection;
-    // }
-    // Same as above
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                // all of this only works if collection is number[]
-                // if collection is an array of numbers:
-                if (this.collection instanceof Array) {
-                    // Type Guard -> collection === number[]
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                // Only going to work if collection is a string
-                // if collection is a string, do this logic instead:
-                if (typeof this.collection === 'string') {
-                    // ~~logic to compare and swap characters in a string
-                }
-                // Type Guard - restore all the properties associated to a string previously missed by the union operator
-            }
-        }
-    }
-}
-// Obs about Type Guards
-// typeof -> narrow type of a value to  primitive type - number, string, boolean, symbol
-// instanceof -> narrow down every other type of value (created with a constructor function)
-const sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-
-const sorter = new Sorter([10, 3, -5, 0])
+const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(sorter.collection);
