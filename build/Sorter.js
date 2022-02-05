a3836dad71da257a92eef173239289bcd023a2b7
Suzana Melo <suzanamelojor@gmail.com>
2022-02-05 18:41:18 +1300

    Sort project: update index to include and use CharactersCollection to sort strings

--- build/Sorter.js
+++ build/Sorter.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
-;
 class Sorter {
     constructor(collection) {
         this.collection = collection;
--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -1,16 +1,16 @@
 export class NumbersCollection {
-  constructor(public data: number[]){}
+  constructor(public data: number[]) {}
 
   // get - used as a keyword to access length value without calling it as a method
-  get length(): number{
+  get length(): number {
     return this.data.length;
   }
 
-  compare(leftIndex:number, rightIndex:number): boolean {
+  compare(leftIndex: number, rightIndex: number): boolean {
     return this.data[leftIndex] > this.data[rightIndex];
   }
 
-  swap(leftIndex:number, rightIndex:number): void {
+  swap(leftIndex: number, rightIndex: number): void {
     const leftHand = this.data[leftIndex];
     this.data[leftIndex] = this.data[rightIndex];
     this.data[rightIndex] = leftHand;
