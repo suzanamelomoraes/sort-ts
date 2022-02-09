6654992b1b53ddb289742c9f49524c16a4563d60
Suzana Melo <suzanamelojor@gmail.com>
2022-02-09 20:47:15 +1300

    Sort project: update index.ts to sort LinkedList

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([1000, 3, 80, -7, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(400);
+linkedList.add(-10);
+linkedList.add(29);
+linkedList.add(77);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
